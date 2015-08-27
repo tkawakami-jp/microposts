61d39137e8a11f0f23b40b1cd35ee3619c90b01d
tkawakami <tkawakami@tkltd.jp>
2015-08-27 06:46:03 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  
   has_secure_password
   
   validates :description, presence: true, length: {minimum: 2, maximum: 140 }, on: :update
   validates :location, presence: true, length: {minimum: 2, maximum: 30 }, on: :update
+  
+  has_many :microposts
 end
